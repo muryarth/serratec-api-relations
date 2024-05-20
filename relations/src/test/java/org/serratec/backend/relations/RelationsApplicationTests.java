e157cbac97d803571c08e01070995a51d46b0af0
muryarth <arthur.mury@outlook.com>
2024-05-20 15:07:38 -0300

    feat: subindo configuração inicial do projeto

---
+++ relations/src/test/java/org/serratec/backend/relations/RelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.relations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
